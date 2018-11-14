daa46a2ba3b6e66f043b8bf51c63d4c56f108831
abc1196 <alejob_11@hotmail.com>
2018-11-13 23:47:45 -0500

    Edited app.py in ci_Server

--- ci_server/app.py
+++ ci_server/app.py
@@ -1,19 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import docker
 import os
 import socket
 
 
 app = Flask(__name__)
 
+client = docker.from_env()
+
 @app.route("/")
 def hello():
     html = "<h3>Hello!</h3>"
     return html
 
-@app.route("/abc/exam2/api/v1/images", methods=["POST"])
+@app.route("/abueno/exam2/api/v1/images", methods=['POST'])
 def build_image():
-    html = "<h3>Hello biatch!</h3>"
-    return html
+    return request.get_data()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
